009cd95320bc8c8fadf5954e5cc298b1d80d3573
7elmi <helmidakkak2005@gmail.com>
2025-07-30 12:40:27 +0300

    Add files via upload

---
+++ EmployeeDB/EmployeeDB/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeDB
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public DateTime EmployeeSince { get; set; }
+        public decimal Salary { get; set; }
+        public int Age { get; set; }
+    }
+}
